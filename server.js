ca23cf73e2e8739af973d07a5a8c149e291289b0
Mario <mmisthree@gmail.com>
2019-04-28 18:16:25 -0700

    heroku

--- server.js
+++ server.js
@@ -21,11 +21,7 @@ app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname + '/client/build'));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  app.use(express.static(__dirname + 'client/build'));
 }
 
 const PORT = process.env.PORT || 3001;
