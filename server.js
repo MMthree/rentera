d148030fbfd6d6fc50cec7336f5a2695dac34863
Mario <mmisthree@gmail.com>
2019-04-28 18:20:52 -0700

    heroku

--- server.js
+++ server.js
@@ -20,9 +20,13 @@ app.use(routes);
 
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname + 'client/build'));
-}
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build')); // serve the static react app
+  app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+  });
+  console.log('Serving React App...');
+};
 
 const PORT = process.env.PORT || 3001;
 
